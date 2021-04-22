d95b7c3fc3c0656eb513d06569b80b750ed3f44d
andrewmoquia <andrewmoquia09@gmail.com>
2021-04-22 12:50:29 +0800

    Add Wildcard Routes

--- src/foods/foods.controller.ts
+++ src/foods/foods.controller.ts
@@ -12,7 +12,51 @@ export class FoodsContoller {
         {
             name: 'banana',
             stock: 10
+        },
+        {
+            name: 'milk',
+            stock: 20
         }
     ]
+  }
+
+  @Get('cook')
+  cook(@Req() request: Request): {} {
+      return [
+        {
+            "Apple Shake": [
+                {
+                    name: 'apple',
+                    stock: 2
+                },
+                {
+                    name: 'milk',
+                    stock: 1
+                }
+            ]
+        },
+        {
+            "Banana Shake": [
+                {
+                    name: 'banana',
+                    stock: 2
+                },
+                {
+                    name: 'milk',
+                    stock: 1
+                }
+            ]
+        }
+
+    ]
+  }
+
+  @Get('/:id')
+  findOne(@Req() request: Request): {} {
+      return {
+          id: 24,
+          name: 'apple',
+          stock: 4
+      }
   }
 }
